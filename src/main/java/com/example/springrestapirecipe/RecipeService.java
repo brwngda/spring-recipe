8834913c4cd9da526221c662f790e048c2b4bf65
brwngda <grz.barwinski@gmail.com>
2023-09-07 14:05:13 +0200

    add recipe service

--- src/main/java/com/example/springrestapirecipe/RecipeService.java
+++ src/main/java/com/example/springrestapirecipe/RecipeService.java
@@ -2,11 +2,34 @@ package com.example.springrestapirecipe;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * @author Grzegorz Barwiński, brwngda
  * @project spring-restapi-recipe
  * @created 9/7/2023
  */
 @Service
-public class RecipeService {
-}
+class RecipeService {
+
+        private final RecipeRepository recipeRepository;
+
+        RecipeService(RecipeRepository recipeRepository) {
+            this.recipeRepository = recipeRepository;
+        }
+
+        List<Recipe> getRecipes() {
+            return recipeRepository.findAll();
+        }
+
+        Recipe addRecipe(Recipe recipe) {
+            return recipeRepository.save(recipe);
+        }
+
+        Recipe deleteRecipe(Long id) {
+            Recipe recipeFromDb = recipeRepository.findById(id)
+                    .orElseThrow();
+            recipeRepository.delete(recipeFromDb);
+            return recipeFromDb;
+        }
+    }
