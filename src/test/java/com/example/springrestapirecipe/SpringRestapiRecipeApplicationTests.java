af8c0e2ea66e8524c7c1fcafbb525020b2996f8a
brwngda <grz.barwinski@gmail.com>
2023-09-07 13:44:44 +0200

    first commit

---
+++ src/test/java/com/example/springrestapirecipe/SpringRestapiRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestapirecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestapiRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
